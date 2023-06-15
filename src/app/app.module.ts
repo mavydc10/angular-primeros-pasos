0aca6a38c9ee942b5613c84ee831475820e4f5a4
Miguel Vargas <mvargas@macronegocios.ec>
2023-06-15 10:53:27 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    DbzModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+ public characterList: Character[] = [{
+  id: uuid(),
+  name: 'Trunks',
+  power: 10000
+ }]
+
+ @Output()
+ public onDelete: EventEmitter<string> = new EventEmitter();
+
+ onDeleteCharacter(id?: string):void{
+
+  if ( !id ) return;
+
+
+  this.onDelete.emit(id);
+ }
+}
